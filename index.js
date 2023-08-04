78d28a8e650ec01429e49b1e3bd39c97571f8542
Rob Tuck <rctuck@princeton.edu>
2023-08-04 18:23:23 -0400

    import Headers?

--- index.js
+++ index.js
@@ -37,10 +37,10 @@ try {
 
     const time = new Date().toTimeString();
 
-    // core.setOutput('time', time);
-    // const payload = JSON.stringify(github.context.payload, undefined, 2);
+    core.setOutput('time', time);
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
 
-    // console.log(`The event payload: ${payload}`);
+    console.log(`The event payload: ${payload}`);
     } catch (error) {
     core.setFailed(error.message);
     }
